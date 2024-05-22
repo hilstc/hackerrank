c833fffb2edec5c0f81923b3ef64772c8cf5753d
Carla Hilst <hilstc@tcd.ie>
2024-05-21 22:04:58 -0300

    first commit

---
+++ python/01_simple_array_sum.py
@@ -0,0 +1,34 @@
+# Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer. 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    sum_ar = int(sum(ar))
+    return sum_ar
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
